a233bd86f568d7aceec533b43146b770bb5cbd07
mgb111 <manishbhanushali1101@gmail.com>
2025-08-02 09:18:41 +0530

    c

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from '../App';
 import AdminUserCampaigns from './AdminUserCampaigns';
-import DemoPage from './DemoPage';
 import AdminLeadMagnetCreator from './AdminLeadMagnetCreator';
 import PromotionGuide from './PromotionGuide';
 import SaaSChecklist from './SaaSChecklist';
@@ -13,7 +12,6 @@ const AppRouter: React.FC = () => {
       <Routes>
         <Route path="/*" element={<App />} />
         <Route path="/admin/users" element={<AdminUserCampaigns />} />
-        <Route path="/demo" element={<DemoPage />} />
         <Route path="/admin/creator" element={<AdminLeadMagnetCreator />} />
         <Route path="/promotion-guide" element={<PromotionGuide />} />
         <Route path="/saas-checklist" element={<SaaSChecklist />} />
